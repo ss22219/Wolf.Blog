e082df3b4f9a774a7fab86416c7b265a51025cdf
李健宁 <lijianning@flytexpress.com>
2018-10-04 09:35:49 +0800

    删除views

--- BlogWeb/Controllers/HomeController.cs
+++ BlogWeb/Controllers/HomeController.cs
@@ -31,30 +31,5 @@ namespace BlogWeb.Controllers
 
             return Json(pageInfo);
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
